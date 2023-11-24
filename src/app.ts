48a294c7dae580e63b27cf2ba6397a7cefcb9b27
Gabriel Gomes <97gabrielgomes@gmail.com>
2023-11-23 23:59:06 -0300

    fix: Removed status monitor due to socket usage

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import router from './routes';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
-import statusMonitor from 'express-status-monitor';
 
 dotenv.config();
 
@@ -13,13 +12,11 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
-app.use(statusMonitor);
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Backend API Version 0.1.2!');
 });
 app.use(router, errorHandler);
-
 app.all('*', notFoundHanlder);
 
 export { app };
